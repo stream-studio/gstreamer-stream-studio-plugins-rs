0322d159b15f3524914fab32762ccc4a914d66d3
Ludovic Bouguerra <bouguerra.ludovic@gmail.com>
2022-11-29 22:57:40 +0100

    Gstreamer Studio Plugins Rs skeleton

---
+++ net/rtmpserver/src/lib.rs
@@ -0,0 +1,21 @@
+use gst::glib;
+
+
+
+fn plugin_init(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
+
+    Ok(())
+}
+
+gst::plugin_define!(
+    rtmpserver,
+    env!("CARGO_PKG_DESCRIPTION"),
+    plugin_init,
+    concat!(env!("CARGO_PKG_VERSION"), "-", env!("COMMIT_ID")),
+    // FIXME: MPL-2.0 is only allowed since 1.18.3 (as unknown) and 1.20 (as known)
+    "MPL",
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_REPOSITORY"),
+    env!("BUILD_REL_DATE")
+);
